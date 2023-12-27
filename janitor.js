24334262766f20522a72046d40a2c0cc53df84a7
javrin <jawabiscuit@users.noreply.github.com>
2023-12-27 17:59:09 -0500

    Initial commit

---
+++ janitor.js
@@ -0,0 +1,27 @@
+const illegalCharacterRegex = /[:\?!\|#‘’\'\"\.,+%&\(\)\\/]/g;
+
+/**
+ * Transforms text into a consitent filename.
+ * All characters are lowercased and words separated by dashes.
+ * @param {string} text - Text to transform into a filename.
+ * @return {string} - The text suitable for use as a filename.
+ */
+function textToFilename(text) {
+    return sanitizeText(text)
+        .replace(/ /g, "-").toLowerCase()
+        .replace(/[--]+/g, "-");
+}
+
+/**
+ * Sanitizes the given text, removing unwanted characters.
+ * @param {string} text - The text to sanitize.
+ * @return {string} - The sanitized text, lowercased.
+ */
+function sanitizeText(text) {
+    return text.replace(illegalCharacterRegex, "").toLowerCase();
+}
+
+module.exports = {
+    textToFilename,
+    sanitizeText,
+};
