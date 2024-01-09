cd4221fd0423929435b18b8b93d5e6fce725cecf
javrin <jawabiscuit@users.noreply.github.com>
2024-01-08 23:31:48 -0500

    Create metadata presets

---
+++ presets.js
@@ -0,0 +1,52 @@
+module.exports = {
+    "frontmatter": {
+        "work from": {
+            type: "string",
+            values: [
+                "remote", "on-site", "hybrid",
+            ],
+        },
+        "job type": {
+            type: "string",
+            values: [
+                "full-time", "part-time", "contract", "freelance",
+            ],
+        },
+        "hiring": {
+            type: "boolean",
+            values: [],
+        },
+        "active": {
+            type: "boolean",
+            values: [],
+        },
+        "applied": {
+            type: "datetime",
+            values: [],
+        },
+        "last contact": {
+            type: "date",
+            values: [],
+        },
+        "application sent": {
+            type: "boolean",
+            values: [],
+        },
+    },
+    "inline": {
+        "company": {
+            type: "tag",
+            values: [
+                "#company",
+            ],
+        },
+        "recruiter link": {
+            type: "url",
+            values: [],
+        },
+        "direct link": {
+            type: "url",
+            values: [],
+        },
+    },
+};
